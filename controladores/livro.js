1a89ac3b8365de26ef87a3b2f3b68b8896137c95
Lucas Aires <devlucasaires@gmail.com>
2024-12-16 10:20:19 -0300

    Criando endPoit delete && Adicionando verificações para correção de erros futuros

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const { getTodosLivros, getLivroPorId, insereLivro } = require("../services/livro")
+const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletaLivroPorId } = require("../services/livro")
 
 function getLivros(req, res) {
     try {
@@ -14,8 +14,13 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = req.params.id
-        const livro = getLivroPorId(id)
-        res.send(livro)
+        if(id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -34,9 +39,46 @@ function postLivro (req, res){
     }
 }
 
+function patchLivro (req,res) {
+    try{
+        const id = req.params.id
+        if(id && Number(id)) {
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+    
+    } catch (error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteLivro (req, res) {
+    try{
+        const id = req.params.id
+
+        if(id && Number(id)) {
+            deletaLivroPorId(id)
+            res.send("livro deletado com sucesso")
+        } else {
+            res.status(422)
+            res.send("ID inválido")
+        }
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
 
 module.exports = {
     getLivros,
     getLivro,
-    postLivro
+    postLivro,
+    patchLivro,
+    deleteLivro
 }
--- services/livro.js
+++ services/livro.js
@@ -20,8 +20,28 @@ function insereLivro(livroNovo) {
     fs.writeFileSync("livros.json", JSON.stringify(novaListaLivros))
 }
 
+function modificaLivro(modificacoes, id) {
+    let livrosAutais = JSON.parse(fs.readFileSync("livros.json"))
+    const indiceModificado = livrosAutais.findIndex(livro => livro.id === id)
+
+    const conteudoAlterado = { ...livrosAutais[indiceModificado], ...modificacoes }
+
+    livrosAutais[indiceModificado] = conteudoAlterado;
+
+    fs.writeFileSync("livros.json", JSON.stringify(livrosAutais))
+;}
+
+function deletaLivroPorId(id){
+    const livros = JSON.parse(fs.readFileSync("livros.json"))
+
+    const livrosFiltrados = livros.filter( livro => livro.id!== id )
+    fs.writeFileSync("livros.json", JSON.stringify(livrosFiltrados))
+}
+
 module.exports = {
     getTodosLivros,
     getLivroPorId,
-    insereLivro
+    insereLivro,
+    modificaLivro,
+    deletaLivroPorId
 }
